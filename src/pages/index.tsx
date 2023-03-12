fa4e871b7fc78fdb9d677418e4a6da46a7fef6b4
Martin Mička <hey@martinmicka.me>
2023-03-12 13:13:00 +0100

    Feat: login page in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,46 @@ import Head from 'next/head'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Quizae</title>
-        <meta name="description" content="A Trivia App" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-          <Container className="mt-3">
-              <Row>
-                  <Col>
-                      <h1 className="text-center">Quizae</h1>
-                  </Col>
-              </Row>
-          </Container>
-      </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Quizae</title>
+                <meta name="description" content="A Trivia App"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <Container>
+                <Row className={'justify-content-center align-items-center mt-5'}>
+                    <Col md={6}>
+                        <Card>
+                            <Card.Header as="h2" className="text-center">Quizae</Card.Header>
+                            <Card.Title className="text-center mt-3">Log In</Card.Title>
+                            <Card.Body>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="email">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3" controlId="password">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" />
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Submit
+                                    </Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
 }
