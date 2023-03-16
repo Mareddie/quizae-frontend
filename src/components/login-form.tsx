c7448576f1b163688075561c353d318e39485970
Martin Mička <hey@martinmicka.me>
2023-03-16 20:12:23 +0100

    Feat: simple hook for flat object form

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,22 +1,36 @@
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import useForm from "@/hooks/use-form";
 
 export default function LoginForm() {
+    const { formObject: loginCredentials, onChangeInput, onSubmitForm } = useForm({
+        email: '',
+        password: '',
+    });
+
     return (
         <Card>
             <Card.Header as="h2" className="text-center">Quizae</Card.Header>
             <Card.Title className="text-center mt-3">Log In</Card.Title>
             <Card.Body>
-                <Form>
+                <Form onSubmit={onSubmitForm}>
                     <Form.Group className="mb-3" controlId="email">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" />
+                        <Form.Control
+                            type="email"
+                            name="email"
+                            onChange={onChangeInput}
+                            value={loginCredentials.email} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="password">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" />
+                        <Form.Control
+                            type="password"
+                            name="password"
+                            onChange={onChangeInput}
+                            value={loginCredentials.password} />
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
---
+++ src/hooks/use-form.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useImmer } from 'use-immer';
+
+export type FormState = {
+    formObject: object,
+    onChangeInput: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    onSubmitForm: (event: React.FormEvent<HTMLFormElement>) => void,
+}
+
+const useForm = (formObjectState: object): FormState => {
+    const [formObject, setFormObject] = useImmer(formObjectState);
+
+    const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+
+        setFormObject(draft => {
+            draft[name] = value;
+        });
+    }
+
+    const onSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log('user', formObject);
+    }
+
+    return {
+        formObject,
+        onChangeInput,
+        onSubmitForm,
+    }
+}
+
+export default useForm
