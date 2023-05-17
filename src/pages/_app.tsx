125051933913208e45584a8e76ff06479e8f2e19
Martin Mička <hey@martinmicka.me>
2023-05-17 20:37:57 +0200

    feat: react query devtools setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type {AppProps} from 'next/app'
 import type {NextPage} from 'next'
 import type {ReactElement, ReactNode} from 'react'
 import {Hydrate, QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Head from "next/head"
 import BaseAppLayout from "@/layouts/base-app-layout"
 import React from "react";
@@ -44,6 +45,7 @@ export default function App({Component, pageProps: {session, ...pageProps}}: App
                     </Head>
                     {renderedLayout}
                 </Hydrate>
+                <ReactQueryDevtools />
             </QueryClientProvider>
         </SessionProvider>
     );
