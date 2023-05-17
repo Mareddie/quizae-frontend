112731573390d5b0a955a1c5398bb84e0f8dabae
Martin Mička <hey@martinmicka.me>
2023-05-17 20:20:08 +0200

    feat: QueryClient setup for Next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,10 @@ import {SessionProvider} from 'next-auth/react'
 import type {AppProps} from 'next/app'
 import type {NextPage} from 'next'
 import type {ReactElement, ReactNode} from 'react'
-import Head from "next/head";
-import BaseAppLayout from "@/layouts/base-app-layout";
+import {Hydrate, QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import Head from "next/head"
+import BaseAppLayout from "@/layouts/base-app-layout"
+import React from "react";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
     getLayout?: (page: ReactElement) => ReactNode
@@ -27,16 +29,22 @@ export default function App({Component, pageProps: {session, ...pageProps}}: App
         ? Component.getLayout(<Component {...pageProps} />)
         : getDefaultLayout(<Component {...pageProps} />);
 
+    const [queryClient] = React.useState(() => new QueryClient())
+
     return (
         <SessionProvider session={session}>
-            <Head>
-                <title key="title">Quizae</title>
-                <meta name="description" content="A Trivia App" key="metaDescription"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1" key="metaViewport"/>
-                <meta name="robots" content="noindex,nofollow" key="metaRobots"/>
-                <link rel="icon" href="/favicon.ico" key="favicon"/>
-            </Head>
-            {renderedLayout}
+            <QueryClientProvider client={queryClient}>
+                <Hydrate state={pageProps.dehydratedState}>
+                    <Head>
+                        <title key="title">Quizae</title>
+                        <meta name="description" content="A Trivia App" key="metaDescription"/>
+                        <meta name="viewport" content="width=device-width, initial-scale=1" key="metaViewport"/>
+                        <meta name="robots" content="noindex,nofollow" key="metaRobots"/>
+                        <link rel="icon" href="/favicon.ico" key="favicon"/>
+                    </Head>
+                    {renderedLayout}
+                </Hydrate>
+            </QueryClientProvider>
         </SessionProvider>
     );
 }
