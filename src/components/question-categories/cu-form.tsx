aaf8cbc4755a8982ee12d46d759c9a4855c15f3b
Martin Mička <hey@martinmicka.me>
2023-05-28 14:06:12 +0200

    feat: extracted form component

--- src/components/question-categories/cu-form.tsx
+++ src/components/question-categories/cu-form.tsx
@@ -1,4 +1,4 @@
-import {Formik, FormikValues} from "formik";
+import {Formik, FormikProps, FormikValues} from "formik";
 import React, {FunctionComponent} from "react";
 import {Modal} from "react-bootstrap";
 import Form from "react-bootstrap/Form";
@@ -11,47 +11,53 @@ type ComponentInput = {
     questionCategory?: yup.InferType<typeof QuestionCategorySchema>
 };
 
-const CreateUpdateForm: FunctionComponent<ComponentInput> = ({questionCategory}) => {
+const FormInternal: FunctionComponent<FormikProps<FormikValues>> = (
+    {handleSubmit, values, handleChange, errors}
+) => {
     const variant = useAtomValue(variantAtom);
 
+    return (
+        <Form noValidate onSubmit={handleSubmit}>
+            <Modal.Body>
+                <Form.Group className="mb-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text"
+                                  name="name"
+                                  value={values.name}
+                                  onChange={handleChange}
+                                  isInvalid={!!errors.name}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Priority</Form.Label>
+                    <Form.Control type="number"
+                                  name="priority"
+                                  value={values.priority}
+                                  onChange={handleChange}
+                                  isInvalid={!!errors.priority}
+                    />
+                    <Form.Text className="text-muted">
+                        This information is used for ordering.
+                        The higher the number, the higher order in the list.
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant={variant === 'create' ? 'success' : 'primary'} type={'submit'}>Submit</Button>
+            </Modal.Body>
+        </Form>
+    );
+};
+
+const CreateUpdateForm: FunctionComponent<ComponentInput> = ({questionCategory}) => {
     return (
         <Formik validationSchema={QuestionCategorySchema}
                 initialValues={{
                     name: questionCategory?.name ?? '',
                     priority: questionCategory?.priority ?? '',
                 }}
-                onSubmit={(data) => resolveFormSubmit(data, questionCategory)}>
-            {({handleSubmit, values, handleChange, errors}) => (
-                <Form noValidate onSubmit={handleSubmit}>
-                    <Modal.Body>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control type="text"
-                                          name="name"
-                                          value={values.name}
-                                          onChange={handleChange}
-                                          isInvalid={!!errors.name}
-                            />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3">
-                            <Form.Label>Priority</Form.Label>
-                            <Form.Control type="number"
-                                          name="priority"
-                                          value={values.priority}
-                                          onChange={handleChange}
-                                          isInvalid={!!errors.priority}
-                            />
-                            <Form.Text className="text-muted">
-                                This information is used for ordering.
-                                The higher the number, the higher order in the list.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Button variant={variant === 'create' ? 'success' : 'primary'} type={'submit'}>Submit</Button>
-                    </Modal.Body>
-                </Form>
-            )}
+                onSubmit={(data) => resolveFormSubmit(data, questionCategory)}
+                component={FormInternal}>
         </Formik>
     );
 };
